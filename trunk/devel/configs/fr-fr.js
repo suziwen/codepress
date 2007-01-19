6320214506f0ebc195d712c9fcbdf7cc5169ea4c
pyrou <pyrou@7d263460-1a27-0410-ba18-bbea68d0aec0>
2007-01-19 11:32:16 +0000

    french language file

---
+++ trunk/devel/configs/fr-fr.js
@@ -0,0 +1,24 @@
+config = {}
+config.language = { 
+	perl       : { name : 'Perl',       extensions : 'pl,cgi'         },
+	java       : { name : 'Java',       extensions : 'java'           },
+	javascript : { name : 'JavaScript', extensions : 'js'             },	
+	text       : { name : 'Texte',	    extensions : 'txt'   	  },	
+	html       : { name : 'HTML',       extensions : 'html,htm,xhtml' },
+	css        : { name : 'CSS',        extensions : 'css'			  },
+	generic    : { name : 'G&eacute;n&eacute;rique',    extensions : '' 			  },
+	php        : { name : 'PHP',        extensions : 'php,phtml'      }
+}
+
+config.message =  {
+	browserNotSupported : 'Votre navigateur n\'est pas support&eacute;.\nLa colorisation syntaxique &agrave; &eacute;t&eacute; d&eacute;sactiv&eacute;e.'
+}
+
+config.menu = {
+	untitledFile : 'Code source sans nom',
+	options      : 'Options',
+	languages    : 'Langages',
+	autoComplete : 'Auto-compl&eacute;tion',
+	fullScreen   : 'Plein &eacute;cran',
+	lineNumbers  : 'Num&eacute;roter les lignes'
+}
