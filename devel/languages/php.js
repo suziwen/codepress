e8048f7f08fb50179ed93b133c1f7cebbab7acbf
pyrou <pyrou@7d263460-1a27-0410-ba18-bbea68d0aec0>
2007-02-05 01:07:55 +0000

    "try/catch" snippet added - "null" keyword added

--- devel/languages/php.js
+++ devel/languages/php.js
@@ -12,7 +12,7 @@ Language.syntax = [ // PHP
 	{ input : /(\?&gt;)/g, output : '$1</strong>' }, // .*?>
 	{ input : /(&lt;\?php|&lt;\?=|&lt;\?|\?&gt;)/g, output : '<cite>$1</cite>' }, // php tags
 	{ input : /(\$[\w\.]*)/g, output : '<a>$1</a>' }, // vars
-	{ input : /\b(false|true|and|or|xor|__FILE__|exception|__LINE__|array|as|break|case|class|const|continue|declare|default|die|do|echo|else|elseif|empty|enddeclare|endfor|endforeach|endif|endswitch|endwhile|eval|exit|extends|for|foreach|function|global|if|include|include_once|isset|list|new|print|require|require_once|return|static|switch|unset|use|while|__FUNCTION__|__CLASS__|__METHOD__|final|php_user_filter|interface|implements|extends|public|private|protected|abstract|clone|try|catch|throw|this)\b/g, output : '<u>$1</u>' }, // reserved words
+	{ input : /\b(false|true|and|or|xor|__FILE__|exception|__LINE__|array|as|break|case|class|const|continue|declare|default|die|do|echo|else|elseif|empty|enddeclare|endfor|endforeach|endif|endswitch|endwhile|eval|exit|extends|for|foreach|function|global|if|include|include_once|isset|list|new|print|require|require_once|return|static|switch|unset|use|while|__FUNCTION__|__CLASS__|__METHOD__|final|php_user_filter|interface|implements|extends|public|private|protected|abstract|clone|try|catch|throw|this|null)\b/g, output : '<u>$1</u>' }, // reserved words
 	{ input : /([^:])\/\/(.*?)(<br|<\/P)/g, output : '$1<i>//$2</i>$3' }, // php comments //
 	{ input : /\/\*(.*?)\*\//g, output : '<i>/*$1*/</i>' }, // php comments /* */
 	{ input : /(&lt;!--.*?--&gt.)/g, output : '<big>$1</big>' } // html comments
@@ -20,6 +20,12 @@ Language.syntax = [ // PHP
 
 // activated with tab
 Language.snippets = [
+	{ input : 'case', output : 'case "$0" : break;' },
+	{ input : 'def', output : 'define(\'$0\',\'\');' },
+	{ input : 'echo', output : 'echo \'$0\';' },
+	{ input : 'for', output : 'for($0,,){\n\t\n}' },
+	{ input : 'fore', output : 'foreach($0 as ){\n\t\n}' },
+	{ input : 'foreach', output : 'foreach($0 as ){\n\t\n}' },
 	{ input : 'if', output : 'if($0){\n\t\n}' },
 	{ input : 'ifelse', output : 'if($0){\n\t\n}\nelse{\n\t\n}' },
 	{ input : 'else', output : '}\nelse {\n\t' },
@@ -29,18 +35,14 @@ Language.snippets = [
 	{ input : 'fun', output : 'function $0(){\n\t\n}' },	
 	{ input : 'func', output : 'function $0(){\n\t\n}' },	
 	{ input : 'while', output : 'while($0){\n\t\n}' },
-	{ input : 'for', output : 'for($0,,){\n\t\n}' },
-	{ input : 'fore', output : 'foreach($0 as ){\n\t\n}' },
-	{ input : 'foreach', output : 'foreach($0 as ){\n\t\n}' },
-	{ input : 'echo', output : 'echo \'$0\';' },
-	{ input : 'switch', output : 'switch($0) {\n\tcase "": break;\n\tdefault: ;\n}' },
-	{ input : 'case', output : 'case "$0" : break;' },
 	{ input : 'ret0', output : 'return false;' },
 	{ input : 'retf', output : 'return false;' },
 	{ input : 'ret1', output : 'return true;' },
 	{ input : 'rett', output : 'return true;' },
 	{ input : 'ret', output : 'return $0;' },
-	{ input : 'def', output : 'define(\'$0\',\'\');' },
+	{ input : 'switch', output : 'switch($0) {\n\tcase "": break;\n\tdefault: ;\n}' },
+	{ input : 'try', output : 'try {\n\t$0\n}\ncatch() {}' },
+	{ input : 'php', output : '<?php\n$0&nbsp;\n\n?>' },
 	{ input : '<?', output : 'php\n$0\n?>' }
 ],
 
