8b07ea0858e9373826f8655847a876a42f37ffd3
fermads <fermads@7d263460-1a27-0410-ba18-bbea68d0aec0>
2007-03-20 20:55:34 +0000

    codepress-lite will became codepress

--- trunk/stable/codepress-lite.js
+++ trunk/stable/codepress-lite.js
@@ -11,32 +11,37 @@
 
 CodePress = function(obj) {
 	var self = document.createElement('iframe');
-	self.reference = document.getElementById(obj.id);
-	self.style.height = obj.clientHeight +'px';
-	self.style.width = obj.clientWidth +'px';
-	self.style.display = 'block';
+	self.textarea = obj;
+	self.textarea.disabled = true;
+	self.textarea.style.overflow = 'hidden';
+	self.style.height = self.textarea.clientHeight +'px';
+	self.style.width = self.textarea.clientWidth +'px';
+	self.textarea.style.overflow = 'auto';	
+	self.style.display = 'inline';
 	self.style.border = '1px solid gray';
-
+	
 	self.initialize = function() {
 		self.editor = self.contentWindow.CodePress;
 		self.editor.body = self.contentWindow.document.getElementsByTagName('body')[0];
-		self.editor.setCode(self.reference.value);
+		self.editor.setCode(self.textarea.value);
 		self.editor.syntaxHighlight('init');
 	}
 	
 	self.edit = function(id,language) {
-		if(id) self.reference = document.getElementById(id+'_cp');
-		self.language = (language) ? language : self.reference.className.replace(/ ?cp ?/,'');
+		if(id) self.textarea.value = document.getElementById(id).value;
+		if(!self.textarea.disabled) return;
+		self.language = (language) ? language : self.textarea.className.replace(/ ?cp ?/,'');
 		self.src = cpPath+'modules/codepress.html?engine='+self.getEngine()+'&language='+self.language;
-		self.attachEvent ? self.attachEvent('onload',self.initialize) : self.addEventListener('load',self.initialize,false);
+		if(self.attachEvent) self.attachEvent('onload',self.initialize);
+		else self.addEventListener('load',self.initialize,false);
 	}
 	
 	self.getCode = function() {
-		return self.editor.getCode();
+		return self.textarea.disabled ? self.editor.getCode() : self.textarea.value ;
 	}
 
 	self.setCode = function(code) {
-		self.editor.setCode(code);
+		self.textarea.disabled ? self.editor.setCode(code) : self.textarea.value = code;
 	}
 	
 	self.toogleLinenumbers = function() {
@@ -45,16 +50,18 @@ CodePress = function(obj) {
 	}
 	
 	self.toogleEditor = function() {
-		if(self.style.display=='block') {
+		if(self.style.display=='inline') {
+			self.textarea.disabled = false;
+			self.textarea.value = self.getCode();
 			self.style.display = 'none';
-			self.reference.value = self.getCode();
-			self.reference.style.display = 'block';
+			self.textarea.style.display = 'inline';
 		}
 		else {
-			self.setCode(self.reference.value);
+			self.textarea.disabled = true;
+			self.setCode(self.textarea.value);
 			self.editor.syntaxHighlight('init');
-			self.style.display = 'block';
-			self.reference.style.display = 'none';
+			self.style.display = 'inline';
+			self.textarea.style.display = 'none';
 		}
 	}
 	
@@ -72,47 +79,10 @@ CodePress = function(obj) {
 	return self;
 }
 
-/*
-
-CodePress.tools = {
-	detect : function() {
-		this.engine = 'older';
-		var ua = navigator.userAgent;
-		if(ua.match('MSIE')) this.engine = 'msie';
-		else if(ua.match('KHTML')) this.engine = 'khtml'; 
-		else if(ua.match('Opera')) this.engine = 'opera'; 
-		else if(ua.match('Gecko')) this.engine = 'gecko';
-	},
-	loadStyle : function(selector, declarations) {
-		var style = document.createElement('style');
-		style.type = 'text/css';
-		document.getElementsByTagName('head')[0].appendChild(style);
-		sheet = document.styleSheets[document.styleSheets.length-1];
-		if (sheet.insertRule) sheet.insertRule(selector+' { '+declarations+' }', sheet.cssRules.length);
-		else if (sheet.addRule) sheet.addRule(selector, declarations);
-	},
-*/	
-/*
-	loadScript : function(src) {
-		var node = document.createElement('script');
-		node.src = src;
-		document.getElementsByTagName('head').item(0).appendChild(node);
-		node = null;
-	},
-*/
-/*
-	start : function(ini) { // on dom load
-		(!ini) ? this.c=0 : this.c++ ;
-		if(typeof(document.getElementsByTagName)!='undefined' && document.getElementsByTagName('body')[0]!=null) CodePress.tools.set()
-		else if(this.c < 50) setTimeout('CodePress.tools.start(1)', 200);
-	},
-}
-*/
-
 CodePress.run = function() {
 	cpPath = document.getElementById('cp-script').src.replace('codepress-lite.js',''); // last index of here
 	t = document.getElementsByTagName('textarea');
-	for(var i=0,z=0,n=t.length;i<n;i++) {
+	for(var i=0,n=t.length;i<n;i++) {
 		if(t[i].className.match('cp')) {
 			id = t[i].id;
 			t[i].id = id+'_cp';
@@ -123,7 +93,5 @@ CodePress.run = function() {
 	}
 }
 
-
-//CodePress.tools.loadStyle('.cp','border:1px solid gray;overflow:auto;background:white;');
 if(window.attachEvent) window.attachEvent('onload',CodePress.run);
 else window.addEventListener('DOMContentLoaded',CodePress.run,false);
