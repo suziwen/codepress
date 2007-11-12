edba9f9f10db463716435fb859fed0fe3d865fa3
pyrou <pyrou@7d263460-1a27-0410-ba18-bbea68d0aec0>
2007-11-12 14:07:10 +0000

    use of console into Gotoline plugin

--- trunk/devel v1/plugins/GotoLine.js
+++ trunk/devel v1/plugins/GotoLine.js
@@ -8,19 +8,29 @@ CodePress.Plugins.GotoLine = function(element) {
 		{
 			evt.stop();
 			
-			var line = window.prompt("Goto to line");
+			if(element.console) element.console.log("Goto line called","info");
+			
+			var line = window.prompt("Go to line ?");
 			
 			if(line!=null) {
 			
 				var code = element.editor.engine.getEditor().innerHTML;
 				var splited = code.split(new RegExp(element.editor.engine.ls));
 				
-				line = parseInt(line);
+				var input = line = parseInt(line);
 				
-				if(line > splited.length) line = splited.length;
-				if(line < 1) line = 1;
+				if(line > splited.length) {
+					line = splited.length;
+					if(element.console) element.console.log("Line invalid ("+input+"), changed to "+(line),"warning");
+				}
+				if(line < 1 || !line) {
+					line = 1;
+					if(element.console) element.console.log("Line invalid ("+input+"), changed to "+(line),"warning");
+				}
 				
-				splited[line-1] = element.editor.engine.cc + splited[line-1];
+				if(element.console) element.console.log("Replace carret to line "+(line),"info");
+				
+				splited[line-1] = element.editor.engine.cc.replace("&","&amp;") + splited[line-1];
 				
 				element.editor.engine.getEditor().innerHTML = splited.join(element.editor.engine.ls);
 				element.editor.engine.findCaret();
