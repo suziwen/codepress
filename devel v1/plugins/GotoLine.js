77978c2144d909c6a881b7b0d5fafe33cf803909
pyrou <pyrou@7d263460-1a27-0410-ba18-bbea68d0aec0>
2007-11-13 10:05:30 +0000

    Use of some new Number prototype, and the new Event.fromChar

--- devel v1/plugins/GotoLine.js
+++ devel v1/plugins/GotoLine.js
@@ -1,44 +1,46 @@
 if(!CodePress.Plugins) CodePress.Plugins = {}
 
+Number.prototype.toString = function ()
+{
+	return this+"";
+}
+
+Number.prototype.limit = function(min,max)
+{
+	return (this < min) ? min : (this > max) ? max : this;
+}
+
 CodePress.Plugins.GotoLine = function(element) {
 
 	this.keyHandler = function(evt)
 	{
-		if(evt.charCode==103 && evt.ctrlKey) // CTRL + G
+		if(evt.fromChar("g") && evt.ctrlKey) // from Char
 		{
-			evt.stop();
-			
-			if(element.console) element.console.info("Goto line called");
-			
+			evt.stop();		
+			if(element.console) element.console.info("Goto line called");	
 			var line = window.prompt("Go to line ?");
 			
-			if(line!=null) {
-			
+			if(line!=null)
+			{
 				var code = element.editor.engine.getEditor().innerHTML;
 				var splited = code.split(new RegExp(element.editor.engine.ls));
 				
-				var input = line = parseInt(line);
+				line = parseInt(line);
 				
-				if(line > splited.length) {
-					line = splited.length;
-					if(element.console) element.console.warning("Line invalid ("+input+")","Line changed to "+(line));
-				}
-				if(line < 1 || !line) {
+				if(line.toString()=="NaN") {
+					if(element.console) element.console.warning("Line invalid (not a number)","Line changed to 1");
 					line = 1;
-					if(element.console) element.console.warning("Line invalid ("+input+")","Line changed to "+(line));
 				}
+				else line = line.limit(1,splited.length);
 				
 				if(element.console) element.console.info("Replace carret to line "+(line));
 				
-				splited[line-1] = element.editor.engine.cc.replace("&","&amp;") + splited[line-1];
-				
+				splited[line-1] = element.editor.engine.cc.replace("&","&amp;") + splited[line-1];	
 				element.editor.engine.getEditor().innerHTML = splited.join(element.editor.engine.ls);
 				element.editor.engine.findCaret();
-			
 			}
-		
+			else if(element.console) element.console.warning("Goto line cancelled");
 		}
-		
 	}
 	
 	element.event.add("keypress",this.keyHandler, this);
