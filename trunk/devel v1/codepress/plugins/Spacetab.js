906c041661b4791f6d1ac49a5cdf95e47f63a736
pyrou <pyrou@7d263460-1a27-0410-ba18-bbea68d0aec0>
2008-11-20 11:18:50 +0000

    minor update

--- trunk/devel v1/codepress/plugins/Spacetab.js
+++ trunk/devel v1/codepress/plugins/Spacetab.js
@@ -4,6 +4,7 @@
  * Replace Tab by Spaces
  *
  * Others usage : 
+ *   [element].tabify()
  *   [element].spacify()
  *   [element].useTab()
  *   [element].useSpace()
@@ -35,25 +36,33 @@ jQuery(function($){
 		{
 			element.event.add("keypress",this.keyHandler, this);
 			element.event.add("keydown",this.keyHandler, this);
-			
-			element.event.add("plugins.loaded", function(){
-				if(this.active) this.spacify();
-			}, this);
+						
+			// element.event.add("plugins.loaded", function(){
+				if(this.spacifyOnLoad) this.spacify(true);
+			// }, this);
 			
 			element.extend({
 				useSpace : function() { this.plugins.Spacetab.active = true; },
 				useTab   : function() { this.plugins.Spacetab.active = false; },
-				spacify  : function() {	this.plugins.Spacetab.spacify(); }
+				spacify  : function() {	this.plugins.Spacetab.spacify(); },
+				tabify   : function() {	this.plugins.Spacetab.tabify(); }
 			});
 		}
 		
-		this.spacify = function()
+		this.spacify = function( noreload /*=false*/)
 		{
 			var code = element.editor.engine.getCode().replace(/\t/g,WHITE_SPACE.repeat(this.tabLength));
 			element.editor.engine.setCode(code,true);
-			element.editor.engine.initialize();
+			if(!noreload) element.editor.engine.initialize();
 		}
-
+		
+		this.tabify = function()
+		{
+			var code = element.editor.engine.getCode().replace(new RegExp(WHITE_SPACE.repeat(this.tabLength),"g"),"\t");
+			element.editor.engine.setCode(code,true);
+			if(!noreload) element.editor.engine.initialize();
+		}
+		
 		this.init();
 	}
 });
