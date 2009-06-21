5a8147705abc3cf5fb06084b4a574c610ee1a9da
fermads <fermads@7d263460-1a27-0410-ba18-bbea68d0aec0>
2009-06-21 16:32:00 +0000

    comments

--- trunk/devel/codepress.js
+++ trunk/devel/codepress.js
@@ -8,7 +8,7 @@
  */
 
 /* TO DO
- * - criar Editor.onload(function(){}) e matar o CodePress.onload + criar o class onload:function
+ * - criar Editor.onload(function(){})  (OU Editor.Events.load) e matar o CodePress.onload + criar o class onload:function
  * - analisar se o callback do codepress.options esta executando 1x e se o onload tb
  * - highlight off is not cleaning previous highlighted code (change language to text?) and on is not backing up to the last selected language
  * - fazer testes de highlight on/off
