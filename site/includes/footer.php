68cc0d17d222f45b0525d72910724a62ed619d2e
fermads <fermads@7d263460-1a27-0410-ba18-bbea68d0aec0>
2016-05-11 04:13:18 +0000

    removing footer

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -1,19 +1,5 @@
-	
+
 </div><!--/container-->
 
-<div id="footer">
-	<a href="http://sourceforge.net/"><img src="http://sourceforge.net/sflogo.php?group_id=37044&type=1" border=0 width="88" height="32" /></a>
-    <a href="http://getfirefox.com/" title="Get Firefox - The Browser, Reloaded."><img src="http://www.mozilla.org/products/firefox/buttons/getfirefox_88x31.png" width="88" height="30" border="1" alt="Get Firefox"></a>
-	<a href="http://sourceforge.net/donate/index.php?group_id=186981"><img src="http://images.sourceforge.net/images/project-support.jpg" width="88" height="32" border="0" alt="Support This Project" /> </a>
-</div>
-
-<? if($_SERVER['HTTP_HOST']!="localhost") { ?>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-<script type="text/javascript">
-_uacct = "UA-709567-5";
-urchinTracker();
-</script>
-<? } ?>
-	
 </body>
 </html>
